a3c07938088c45c45e955f6bec406a97109de1bb
Tearfall <108473064+Tearfall@users.noreply.github.com>
2023-09-24 20:06:14 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public bool imageClicked = false;
+        public bool image1Clicked = false;
+        public string expect = "EXPECTATION";
+        public string reality = "REALITY";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onImageButton1Clicked(object sender, EventArgs e)
+        {
+            if (imageClicked)
+            {
+                imagebutton1.Source = "expectation1.jpg";
+                label1.Text = expect;
+            }
+            else
+            {
+                imagebutton1.Source = "reality1.jpg";
+                label1.Text = reality;
+            }
+            imageClicked = !imageClicked;
+
+        }
+
+        public void onImageButton2Clicked(object sender, EventArgs e)
+        {
+            if (image1Clicked)
+            {
+                imagebutton2.Source = "expectation2.jpg";
+                label2.Text = expect;
+            }
+            else
+            {  
+                imagebutton2.Source = "reality2.jpg";
+                label2.Text= reality;
+            }
+            image1Clicked = !image1Clicked;
+        }
+    }
+}
